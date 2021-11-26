09353d06179e057454996fc97e27dec1fced916a
Imran <imran2244556677@gmail.com>
2021-11-26 23:57:00 +0600

    profile screen and get user details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 const App = () => {
     return (
@@ -23,6 +24,7 @@ const App = () => {
                         <Route path="/cart/:id?" component={CartScreen} />
                         <Route path="/login" component={LoginScreen} />
                         <Route path="/register" component={RegisterScreen} />
+                        <Route path="/profile" component={ProfileScreen} />
                     </Container>
                 </main>
                 <Footer />
