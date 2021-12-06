5da483f3d590441addc20c8a26122fdea0cc9ac5
Imran <imran2244556677@gmail.com>
2021-12-06 18:11:11 +0600

    delete user by admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { authUser, getUserProfile, getUsers, registerUser, updateUserProfile } from "../controllers/userController.js";
+import { authUser, deleteUser, getUserProfile, getUsers, registerUser, updateUserProfile } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
@@ -9,5 +9,6 @@ router.post("/login", authUser);
 router.route("/profile")
     .get(protect, getUserProfile)
     .put(protect, updateUserProfile);
+router.route("/:id").delete(protect, admin, deleteUser);
 
 export default router;
--- frontend/src/screens/UserListScreen.js
+++ frontend/src/screens/UserListScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
-import { listUsers } from "../actions/userActions";
+import { listUsers, deleteUser } from "../actions/userActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import { Button, Table } from "react-bootstrap";
@@ -15,16 +15,21 @@ const UserListScreen = ({ history }) => {
     const userLogin = useSelector((state) => state.userLogin);
     const { userInfo } = userLogin;
 
+    const userDelete = useSelector((state) => state.userDelete);
+    const { success: successDelete } = userDelete;
+
     useEffect(() => {
         if (userInfo && userInfo.isAdmin) {
             dispatch(listUsers());
         } else {
             history.push("/login");
         }
-    }, [dispatch, history]);
+    }, [dispatch, history, userInfo, successDelete]);
 
     const deleteHandler = (id) => {
-
+        if (window.confirm("Are you sure?")) {
+            dispatch(deleteUser(id));
+        }
     };
 
     return (
