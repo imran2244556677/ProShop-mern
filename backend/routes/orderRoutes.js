05ab24ede20ca18cb8eded66d48cda11aa6a97e1
Imran <imran2244556677@gmail.com>
2021-12-07 23:10:02 +0600

    admin order list

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders } from "../controllers/orderController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders } from "../controllers/orderController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(protect, addOrderItems);
+
+router.route("/").post(protect, addOrderItems).get(protect, admin, getOrders);
 router.get("/myorders", protect, getMyOrders);
 router.route("/:id").get(protect, getOrderById);
 router.route("/:id/pay").put(protect, updateOrderToPaid);
