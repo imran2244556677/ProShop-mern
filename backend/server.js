8e4aab4f5197bf13dd447e7d4b0d2b02d7afa66d
Imran <imran2244556677@gmail.com>
2021-12-10 19:25:34 +0600

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,12 @@ connectDB();
 
 const app = express();
 
-if (process.env.NODE_ENV === "Development") {
+if (process.env.NODE_ENV === "development") {
     app.use(morgan("dev"));
 }
 
 app.use(express.json());
 
-
-app.get("/", (req, res) => {
-    res.send("API is running");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -35,8 +31,22 @@ app.use("/api/upload", uploadRoutes);
 
 app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
+
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend/build", "index.html"));
+    });
+} else {
+    app.get("/", (req, res) => {
+        res.send("API is running");
+    });
+}
+
 
 app.use(notFound);
 app.use(errorHandler);
